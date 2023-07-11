ba412c2ef325127abe66278e59b119e7d2fd7c4b
Biz Dev <43341818+bizdev324@users.noreply.github.com>
2023-07-11 03:23:33 -0700

    resolved the cron job issue

---
+++ src/cronManager/jobs/setOpenPositions.js
@@ -0,0 +1,48 @@
+const setOpenPositions = async (exchange, openPositions) => {
+  try {
+    const securities = await exchange.fetchPositions();
+    let actions = [];
+
+    // Find new positions.
+    securities.forEach(security => {
+      const posIndex = openPositions.findIndex(obj => obj.timestamp === security.timestamp);
+  
+      if(posIndex === -1) {
+        const newPosition = {
+          timestamp: security.timestamp,
+          tradeSize: security.contractSize * security.contracts,
+          tradePair: security.symbol,
+          profit: security.unrealizedPnl,
+          side: security.side,
+        };
+        openPositions.push(newPosition);
+  
+        // Added the action to send to the frontend.
+        actions.push({
+          type: 'insert',
+          position: newPosition,
+        });
+      }
+    });
+  
+    //Find the positions which should be removed.
+    for(let i = 0; i < openPositions.length; i ++) {
+      const openPosIndex = securities.findIndex(obj => obj.timestamp === openPositions[i].timestamp);
+      if(openPosIndex === -1) {
+        actions.push({
+          type: 'remove',
+          position: openPositions[i]
+        });
+        
+        openPositions.splice(i, 1);
+        i --;
+      }
+    }
+    
+    return actions;
+  } catch (error) {
+    console.log('Cron_SetOpenPosition', error);
+  }
+}
+
+export default setOpenPositions;
---
+++ src/modules/Position/model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const positionSchema = new Schema(
+  {
+    tradePair: {
+      type: String,
+    },
+    tradeSize: {
+      type: Number,
+      default: 0,
+    },
+    profit: {
+      type: Number,
+      default: 0,
+    },
+    side: {
+      type: String,
+    },
+    timestamps: {
+      type: Number,
+    }
+  },
+  {
+    autoIndex: true,
+    timestamps: true,
+    toJSON: { getters: true },
+  }
+);
+const PositionModel = mongoose.model("Position", positionSchema, "positions");
+
+export default PositionModel;
